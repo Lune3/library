da74e33e5981bc632b8fea32ba498bf008efd67c
Lune3 <113820374+Lune3@users.noreply.github.com>
2024-02-09 22:41:51 +0530

    Made all the book input to go in book array

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 const books = [];
 
+function Book(author,title,pages){
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+}
+
 const addButton = document.querySelector(".addBooks");
 const dialog = document.querySelector("dialog");
 addButton.addEventListener(("click"),() =>{
     dialog.showModal();
 })
+
+const submitButton = document.querySelector("#confirmBtn");
+
+submitButton.addEventListener(("click"),(e) => {
+    e.preventDefault();
+    dialog.close();
+    appendBook();
+})
+
+function appendBook(){
+    const authorName = document.querySelector("#author");
+    const bookTitle = document.querySelector("#title");
+    const pages = document.querySelector("#pages");
+    const book = new Book(authorName.value,bookTitle.value,pages.value);
+    books.push(book);
+    displayChild(authorName.value,bookTitle.value,pages.value);
+}
+
+
+
+
+
 
