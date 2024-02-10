2a52dca2b5db031ad72f9091384341c405c3fe58
Lune3 <113820374+Lune3@users.noreply.github.com>
2024-02-10 19:24:49 +0530

    Added book to appear in section when confirm pressed

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const books = [];
 
-function Book(author,title,pages){
+function Book(author,title,pages,read){
     this.author = author;
     this.title = title;
     this.pages = pages;
+    this.read = read;
 }
+
+const authorName = document.querySelector("#author");
+const bookTitle = document.querySelector("#title");
+const pages = document.querySelector("#pages");
+const read = document.querySelector("#read");
 
 const addButton = document.querySelector(".addBooks");
 const dialog = document.querySelector("dialog");
@@ -21,12 +27,16 @@ submitButton.addEventListener(("click"),(e) => {
 })
 
 function appendBook(){
-    const authorName = document.querySelector("#author");
-    const bookTitle = document.querySelector("#title");
-    const pages = document.querySelector("#pages");
-    const book = new Book(authorName.value,bookTitle.value,pages.value);
+    const book = new Book(authorName.value,bookTitle.value,pages.value,read.value);
     books.push(book);
-    displayChild(authorName.value,bookTitle.value,pages.value);
+    displayChild(authorName.value,bookTitle.value,pages.value,read.value);
+}
+
+function displayChild(authorName,bookTitle,pages,read){
+    const template = document.querySelector("template").content.cloneNode(true);
+    const section = document.querySelector("section");
+    section.appendChild(template);
+        
 }
 
 
