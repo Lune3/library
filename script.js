7133a4b1a5881bc559d052b1e51b18184a0a601b
Lune3 <113820374+Lune3@users.noreply.github.com>
2024-02-12 20:04:29 +0530

    Fixing book name updating first book

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const authorName = document.querySelector("#author");
 const bookTitle = document.querySelector("#title");
 const pages = document.querySelector("#pages");
 const read = document.querySelector("#read");
+const submitButton = document.querySelector("#confirmBtn");
 
 const addButton = document.querySelector(".addBooks");
 const dialog = document.querySelector("dialog");
@@ -18,26 +19,24 @@ addButton.addEventListener(("click"),() =>{
     dialog.showModal();
 })
 
-const submitButton = document.querySelector("#confirmBtn");
-
 submitButton.addEventListener(("click"),(e) => {
     e.preventDefault();
     dialog.close();
-    appendBook();
-})
-
-function appendBook(){
     const book = new Book(authorName.value,bookTitle.value,pages.value,read.value);
     books.push(book);
     displayChild(authorName.value,bookTitle.value,pages.value,read.value);
-}
+})
 
-function displayChild(authorName,bookTitle,pages,read){
+function displayChild(){
     const template = document.querySelector("template").content.cloneNode(true);
     const section = document.querySelector("section");
     section.appendChild(template);
-        
+    const description = document.querySelectorAll("p");
+    const bookAdded = books[books.length - 1];
+    description[0].textContent = `${bookAdded.author}`;
 }
+
+
 
 
 
